3ee8d11684dee112ac4b0ae5459c94a0115f3be7
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-23 16:06:35 +0530

    dfgb

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
