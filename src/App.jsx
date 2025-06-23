7cf2a4eba7ec5587b7c7a91d493515355da55fb4
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-23 09:32:54 +0530

    hgf

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
